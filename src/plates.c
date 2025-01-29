4cd47d7e68f033a6ea6e24aa83d3acdd9f9ddfe2
Joel Danielewicz <danielewiczjoel@gmail.com>
2025-01-29 11:34:18 -0800

    added texture cycleing using 'c' keybind

--- src/plates.c
+++ src/plates.c
@@ -9,7 +9,7 @@
 void GenerateTerrain(TerrainData* data) {
     // alloc memory
     data->height_map = MemAlloc(sizeof(float) * (data->width + 1) * (data->height + 1));
-    data->image_count = 1; // currenly no texture data, 
+    data->image_count = 2; // currenly no texture data, 
     data->images = MemAlloc(sizeof(Image) * data->image_count);
     data->textures = MemAlloc(sizeof(Texture2D) * data->image_count);
 
@@ -26,6 +26,28 @@ void GenerateTerrain(TerrainData* data) {
 
     data->images[0] = GenImageChecked(data->width, data->height, 1, 1, PINK, ORANGE);
     data->textures[0] = LoadTextureFromImage(data->images[0]);
+
+    Color *pixels = (Color *)malloc(data->width * data->height * sizeof(Color));
+
+    for (int y = 0; y < data->height; y++)
+    {
+        for (int x = 0; x < data->width; x++)
+        {
+            if (((x/32+y/32)/1)%2 == 0) pixels[y*data->width + x] = ORANGE;
+            else pixels[y*data->width + x] = GOLD;
+        }
+    }
+
+    // Load pixels data into an image structure and create texture
+    data->images[1] = (Image){
+        .data = pixels,             // We can assign pixels directly to data
+        .width = data->width,
+        .height = data->height,
+        .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+        .mipmaps = 1
+    };
+
+    data->textures[1] = LoadTextureFromImage(data->images[1]);
 
 }
 
